73305a9786681918e142c2ff8abb0f04db6f241a
Nitish Choubey <nitishchoubey1234@gmail.com>
2025-09-06 17:17:29 +0530

    UPDATE: Adding Authentication part

---
+++ TradeShield/backend/src/app.js
@@ -0,0 +1,17 @@
+// src/app.js
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("🚀 TradeShield API is running...");
+});
+
+module.exports = app;
